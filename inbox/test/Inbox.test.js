d77a7f03ed7f682639944c85865ee26d18a063b6
ehud pinhas <ehudpinhas@ehuds-MacBook-Pro.local>
2018-10-17 06:36:26 +0300

    add test

--- inbox/test/Inbox.test.js
+++ inbox/test/Inbox.test.js
@@ -3,6 +3,8 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 const { interface, bytecode } = require('../compile');
+const InitialMessage='Hello World';
+const MessageChange='MessageChange';
 
 let accounts;
 let inbox;
@@ -17,7 +19,7 @@ beforeEach(async () => {
     //use one of this accounts to deploy
     //the contract
     inbox = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['hello world'] })
+        .deploy({ data: bytecode, arguments: [InitialMessage] })
         .send({ from: accounts[0], gas: '1000000' });
 
 
@@ -25,8 +27,19 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
     it('display a contract', () => {
-        console.log(inbox);
+        assert.ok(inbox.options.address);
     });
+
+    it('has default message', async() => {
+     const message=await inbox.methods.message().call();
+     assert.equal(message,InitialMessage);
+    });
+
+    it('can change message', async() => {
+        await inbox.methods.setMessage(MessageChange).send({from:accounts[0]});
+        const message=await inbox.methods.message().call();
+        assert.equal(message,MessageChange);
+       });
 });
 
 
